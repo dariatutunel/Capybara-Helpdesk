b402f385a10b8f6d192af1ac422874d4794c4881
Daria Tutunel <dariatutunel26@gmail.com>
2026-04-14 16:14:22 +0300

    Added Sign up and Login page

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session
 import mysql.connector
 
 app=Flask(__name__)
+app.secret_key='capy123'
 
 def get_db_connection():
     connection=mysql.connector.connect(
@@ -12,16 +13,80 @@ def get_db_connection():
         port=3307
     )
     return connection
+    
 
 @app.route('/')
 def index():
+    if 'user_id' not in session:
+        return redirect(url_for('login'))
     conn=get_db_connection()
     cursor=conn.cursor(dictionary=True)
     cursor.execute("SELECT * FROM tickets")
     tickets_found=cursor.fetchall()
     cursor.close()
     conn.close()
-    return render_template('index.html',tickets=tickets_found)
+    return render_template('index.html',tickets=tickets_found, username=session.get('username'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method=='POST':
+        username=request.form['username']
+        password=request.form['password']
+        conn=get_db_connection()
+        cursor=conn.cursor(dictionary=True)
+        cursor.execute("SELECT * FROM users WHERE username=%s AND password=%s", (username, password))
+        user=cursor.fetchone()
+        cursor.close()
+        conn.close()
+        if user:
+            session['user_id']=user['id']
+            session['username']=user['username']
+            return redirect(url_for('index'))
+        return "Invalid information!"
+    return render_template('login.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method=='POST':
+        username=request.form['username']
+        password=request.form['password']
+        conn=get_db_connection()
+        cursor=conn.cursor()
+        try:
+            cursor.execute("INSERT INTO users(username,password) VALUES (%s, %s)",(username, password))
+            conn.commit()
+            return redirect(url_for('login'))
+        except:
+            return "Username already exists!"
+        finally:
+            cursor.close()
+            conn.close()
+    return render_template('signup.html')
+        
+
+@app.route('/add', methods=['GET','POST'])
+def add_ticket():
+    if 'user_id' not in session:
+        return redirect(url_for('login'))
+    if request.method=='POST':
+        title=request.form['title']
+        description=request.form['description']
+        user_id=request.form['user_id']
+        conn=get_db_connection()
+        cursor=conn.cursor()
+        cursor.execute(
+            "INSERT INTO tickets(title, description, user_id) VALUES (%s, %s, %s)",(title, description, user_id)
+        )
+        conn.commit()
+        cursor.close()
+        conn.close()
+        return redirect(url_for('index'))
+    return render_template('add_ticket.html')
+        
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('login'))
 
 if __name__=='__main__':
     app.run(debug=True)
--- database.sql
+++ database.sql
@@ -1,20 +1,17 @@
 CREATE DATABASE ticket_system;
 USE ticket_system;
-CREATE TABLE users (
+
+CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(25) NOT NULL
 );
-CREATE TABLE tickets (
+
+CREATE TABLE IF NOT EXISTS tickets (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(100) NOT NULL,
-    description TEXT NOT NULL,
-    status VARCHAR(20) DEFAULT 'Open',
+    description TEXT,
     user_id INT,
-    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+    status VARCHAR(50) DEFAULT 'Open',
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
-INSERT INTO users(name, email) VALUES ('Ion Popa', 'ion_popa123@yahoo.com');
-INSERT INTO users(name, email) VALUES ('Maria Iordache', 'iordache_m@gmail.com');
-INSERT INTO tickets(title, description, user_id) VALUES('Login button is not working', 'When I press Login the page keeps loading', '1');
-INSERT INTO tickets(title, description, user_id) VALUES('Wrong font color', 'On the contact page, the text is gray instead of black', '2');
